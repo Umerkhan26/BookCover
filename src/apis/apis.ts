6eeca654cbc1f0ebd21e0a794031d73d4d55d6ef
Umerkhan26 <114725165+Umerkhan26@users.noreply.github.com>
2025-03-08 03:16:57 +0500

    updated

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+// const API_BASE_URL = "http://13.60.184.141/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const registerUser = async (userData: {
   firstName: string;
